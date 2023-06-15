f9e67bf5e7c82fb06618f9b003d5652b0204db85
Azeez Saka <saoconcept1@gmail.com>
2023-06-15 08:09:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,13 @@
-/*
- * File: 4-puts.c
- * Author: Azeez Saka
- * Date: June 15, 2023
- * Description: This program is like building a multilingual puzzle
- */
 #include <stdio.h>
-/*
- * Function: main
- * --------------------
- * This is the entry point of the program.
- * --------------------
- * Parameters:
- *     void
- * --------------------
- * Returns:
- *     The integer value '0'.
+/**
+ * main -Entry point
+ * ------------------
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 /* Print out exactly "Programming is like building a multilingual puzzl*/
 	puts("\"Programming is like building a multilingual puzzle");
 /* Return value '0' if successful*/
-	return 0;
+	return (0);
 }
