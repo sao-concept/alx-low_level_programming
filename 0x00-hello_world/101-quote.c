8d3342db6a3fbcacb6cb1ff7c2b49fc2f53c5869
Azeez Saka <saoconcept1@gmail.com>
2023-06-15 20:49:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
-/*
- * main -Print "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+/**
+ * main -Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19")
  * then a newline is printed to standard error.
  * ------------------
  * Return: Always 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\"
-			- Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59);
 	return (1);
 }
