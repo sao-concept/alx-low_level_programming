7f5ff13f3eac51c47188190a898f3395da3b9afc
Azeez Saka <saoconcept1@gmail.com>
2023-06-16 15:29:01 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * ------------------
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i > 0)
+	{
+		printf("%d is a base_10_number\n", i);
+		i++;
+	}
+	return (0);
+}
