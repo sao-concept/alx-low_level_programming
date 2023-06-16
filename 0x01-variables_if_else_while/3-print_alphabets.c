0fb361bb91c7ccca64d75e60dabf8f87d9421c53
Azeez Saka <saoconcept1@gmail.com>
2023-06-16 14:20:46 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * ------------------
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowerAlpha;
+	char upperAlpha;
+
+	for (lowerAlpha = 'a'; lowerAlpha <= 'z'; lowerAlpha++)
+	{
+		putchar(lowerAlpha);
+	}
+		for (upperAlpha = 'A'; upperAlpha <= 'Z'; upperAlpha++)
+		{
+			putchar(upperAlpha);
+		}
+	putchar('\n');
+	return (0);
+}
