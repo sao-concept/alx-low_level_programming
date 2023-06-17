6a92ad58e64183779ad0eca4a5a11b0d1e6f5212
Azeez Saka <saoconcept1@gmail.com>
2023-06-17 08:33:44 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,30 +2,38 @@
  * main -Entry point
  * ---------------------
  * Author: Azeez Saka
- * Date: <16-06-2023>
+ * Date: <17-06-2023>
  * Return: Always 0 (Success)
  */
 
-/* Program that prints all possible different combinations of two digits. */
-
+// Include the necessary headers
 #include <stdio.h>
 
 int main(void)
 {
+/* Program that prints all possible different combinations of three  digits. */
+
 	int firstDig;
 	int secondDig;
+	int thirdDig;
 
 	for (firstDig = 0; firstDig <= 8; firstDig++)
 	{
 		for (secondDig = firstDig + 1; secondDig <= 9; secondDig++)
 		{
+			for (thirdDig = secondDig + 1; thirdDig <= 9;
+					thirdDig++)
+			{
+
 			putchar((firstDig % 10) + '0');
 			putchar((secondDig % 10) + '0');
+			putchar((thirdDig % 10) + '0');
 
-			if (firstDig != 8 || secondDig != 9)
+			if (firstDig != 8 || secondDig != 9 || thirdDig != 10)
 			{
 				putchar(',');
 				putchar(' ');
+			}
 			}
 		}
 	}
