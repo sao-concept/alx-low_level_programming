fda53da3c81bea4074d5fedd706d6af8d748dbe7
Azeez Saka <saoconcept1@gmail.com>
2023-06-17 09:15:26 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,16 @@
+
+/**
+ * main -Entry point
+ * ---------------------
+ * Author: Azeez Saka
+ * Date: <17-06-2023>
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    // Your code here
+    return (0);
+}
