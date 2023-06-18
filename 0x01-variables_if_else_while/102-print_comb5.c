cda74d636894c45796fb3b1673fefee0a62d2a50
Azeez Saka <saoconcept1@gmail.com>
2023-06-17 20:36:46 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,3 @@
-
 /**
  * main -Entry point
  * ---------------------
@@ -9,8 +8,35 @@
 
 #include <stdio.h>
 
+/*
+ * Program that prints all possible combinations of two two-digit numbers from
+ * 0-99 which are separated by comma, followed by space
+ */
+
 int main(void)
 {
-    // Your code here
-    return (0);
+	int digSet1;
+	int digSet2;
+
+	for (digSet1 = 0; digSet1 < 100; digSet1++)
+	{
+		for (digSet2 = 0; digSet2 < 100; digSet2++)
+		{
+			if (digSet1 < digSet2)
+			{
+				putchar((digSet1 / 10) + 48);
+				putchar((digSet1 % 10) + 48);
+				putchar(' ');
+				putchar((digSet2 / 10) + 48);
+				putchar((digSet2 % 10) + 48);
+				if (digSet1 != 98 || digSet2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+		return (0);
 }
