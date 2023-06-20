8b780d77a9a8673368d9c80d99b5c1d5e3d2df5b
Azeez Saka <saoconcept1@gmail.com>
2023-06-20 12:59:43 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 /**
-* main -Entry point
+* print_alphabet -Entry point
 * ---------------------
 * Author: Azeez Saka
-* Date: <current date>
+* Date: <20-06-2023>
 * Return: Always 0 (Success)
 */
 
 /* Include the necessary headers */
 #include "main.h"
-
 void print_alphabet(void)
 {
 char lCase = 'a';
