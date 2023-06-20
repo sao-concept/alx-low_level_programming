173adee999b1251c82b3941bc790a2c8d09e4ff0
Azeez Saka <saoconcept1@gmail.com>
2023-06-20 06:34:46 -0700

    main.h and _putchar.c files

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,19 @@
+/**
+* _putchar -Custom Function
+* ---------------------
+* Author: Azeez Saka
+* Date: <20-06-2023>
+* Return: Always 0 (Success)
+*/
+
+/* creation of a custom function named '_putchar' */
+
+#ifndef _PUTCHAR_H /*HEADER GUARD for _PUTCHAR_H */
+
+#define _PUTCHAR_H
+
+int _putchar(char var_name); /* Function declaration for _putchar(prototype) */
+
+return (var_name); /* Return character */
+
+#endif /* EOF */
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char var_name);
+
+/* Other function prototypes can be added here */
+
+#endif
