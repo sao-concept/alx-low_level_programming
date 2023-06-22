3484ee65ae1de5f14e648633a073ef26eb6286a7
Azeez Saka <saoconcept1@gmail.com>
2023-06-22 06:55:04 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -21,6 +21,7 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 int _isupper(int c);
+int _isdigit(int c);
 
 
 /* Other function prototypes can be added here */
