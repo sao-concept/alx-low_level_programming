69f684de2357e1df6d220f3e5fb7dc1aa55b9db7
Azeez Saka <saoconcept1@gmail.com>
2023-06-22 07:30:40 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -22,7 +22,7 @@ int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 int _isupper(int c);
 int _isdigit(int c);
-
+int mul(int a, int b);
 
 /* Other function prototypes can be added here */
 
