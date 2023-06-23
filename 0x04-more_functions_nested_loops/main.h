eef7e289e6e0b3b1a18d36a5df0aa90cf3bf67eb
Azeez Saka <saoconcept1@gmail.com>
2023-06-23 02:14:35 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -28,6 +28,7 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_square(int size);
 
 /* Other function prototypes can be added here */
 
