442e3bdb8ac2482786afbf0b619a4ea22883bed0
Azeez Saka <saoconcept1@gmail.com>
2023-06-22 22:52:06 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,15 +11,14 @@
 void more_numbers(void)
 {
 int i, pCount;
+
 for (pCount = 0; pCount < 10; pCount++)
 {
 for (i = 0; i < 15; i++)
 {
 if (i >= 10)
-{
 _putchar(i / 10 + '0');
 _putchar(i % 10 + '0');
-}
 }
 _putchar('\n');
 
