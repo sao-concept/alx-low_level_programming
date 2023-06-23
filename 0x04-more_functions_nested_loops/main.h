f734c2bd1bc4f59005e3bf01716998b7c096c4b4
Azeez Saka <saoconcept1@gmail.com>
2023-06-23 02:30:39 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -29,6 +29,7 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
 
 /* Other function prototypes can be added here */
 
