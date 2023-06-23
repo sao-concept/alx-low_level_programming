d30b3b698f4bcb102b937adc493251283e54a60f
Azeez Saka <saoconcept1@gmail.com>
2023-06-23 01:43:01 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -27,6 +27,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
 
 /* Other function prototypes can be added here */
 
