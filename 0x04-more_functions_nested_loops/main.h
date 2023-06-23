b5c3b395209cc217783b6930f84ab1afcb63fcfa
Azeez Saka <saoconcept1@gmail.com>
2023-06-23 02:57:43 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -30,7 +30,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
+void print_number(int n);
 /* Other function prototypes can be added here */
 
 #endif /* MAIN_H */
