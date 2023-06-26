b12d35a5c697181eccbcf671f34b8bc4c7af7e69
Azeez Saka <saoconcept1@gmail.com>
2023-06-26 08:45:33 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -34,6 +34,7 @@ void print_number(int n);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 /* Other function prototypes can be added here */
 
 #endif /* MAIN_H */
