f9fc88e0aabecb1c643b352aff8458c5a2d9b556
Azeez Saka <saoconcept1@gmail.com>
2023-06-26 15:25:05 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -41,6 +41,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 /* Other function prototypes can be added here */
 
 #endif /* MAIN_H */
