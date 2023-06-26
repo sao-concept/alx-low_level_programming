9e326b0d5b85ecbb7bf332eec76bd6d204acae0c
Azeez Saka <saoconcept1@gmail.com>
2023-06-26 15:34:05 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,39 @@
+
+/**
+* *generate_password -This program generate random valid password
+* @*password: parameter
+* Return: Always 0 (Success)
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+char *generate_password()
+{
+static const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+char *password = malloc((PASSWORD_LENGTH + 1) * sizeof(char)); /* +1 for the null terminator */
+
+srand(time(NULL));
+
+for (int i = 0; i < PASSWORD_LENGTH; i++)
+{
+password[i] = charset[rand() % (sizeof(charset) - 1)];
+}
+
+password[PASSWORD_LENGTH] = '\0'; /* Null-terminate the password */
+
+return (password);
+}
+
+int main(void) {
+char *password = generate_password();
+
+printf("%s\n", password);
+
+free(password);
+
+return (0);
+}
+
