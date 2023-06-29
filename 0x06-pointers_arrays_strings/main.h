e278bcce2010a3e05c2b2e1ff92b31d1337a6ec5
Azeez Saka <saoconcept1@gmail.com>
2023-06-29 02:30:25 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -45,6 +45,7 @@ int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 /* Other function prototypes can be added here */
 
 #endif /* MAIN_H */
