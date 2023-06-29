a420360d8bb7a7ea539df2197ca3a27feb1a328a
Azeez Saka <saoconcept1@gmail.com>
2023-06-29 02:45:46 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -46,6 +46,7 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
 /* Other function prototypes can be added here */
 
 #endif /* MAIN_H */
