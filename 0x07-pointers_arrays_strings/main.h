a6c9eee8f0369050e11ebb8c6c03b1259e93884f
Azeez Saka <saoconcept1@gmail.com>
2023-07-03 04:02:15 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -55,6 +55,7 @@ void print_buffer(char *b, int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 /* Other function prototypes can be added here */
 
 #endif /* MAIN_H */
