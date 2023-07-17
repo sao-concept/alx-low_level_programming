5fd85daf9e2418b6a30267adcc9951cde04a66ab
Azeez Saka <saoconcept1@gmail.com>
2023-07-17 06:21:50 -0700

    add 4-new_dog.c file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,13 @@
 #define DOG
 
 /**
-* struct dog -Dog Info
+* struct dog -This is a struct for  dog
 *
-* @name: Dog name
+* @name: dog name
 *
-* @age: Dog age
+* @age: dog age
 *
-* @owner: Dog Owner
+* @owner: dog Owner name
 */
 
 struct dog
@@ -18,14 +18,6 @@ float age;
 char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
-typedef struct dog dog_t;
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
