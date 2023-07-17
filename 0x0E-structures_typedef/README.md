37f5a77ff88254e73c32183bd6dc7401b9a2ad2b
Azeez Saka <saoconcept1@gmail.com>
2023-07-17 05:44:17 -0700

    add dog.h and README file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is a directory for the task 0x0E-structures_typedef on struct
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG
+#define DOG
+
+/**
+* struct dog -Dog Info
+*
+* @name: Dog name
+*
+* @age: Dog age
+*
+* @owner: Dog Owner
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif /* DOG */
