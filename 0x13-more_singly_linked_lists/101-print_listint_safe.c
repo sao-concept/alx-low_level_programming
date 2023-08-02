83977ce917af9253152529946eeb18d9b2151d06
Azeez Saka <saoconcept1@gmail.com>
2023-08-02 05:14:23 -0700

    update 101-print_listint_safe.c file

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,47 +1,29 @@
 #include "lists.h"
 
-
 /**
 * print_listint_safe -This function that prints a listint_t linked list.
-* @head: list head.
+* @head: list head
 *
-* Return: number of nodes in list.
+* Return: number of nodes in list
 */
 size_t print_listint_safe(const listint_t *head)
 {
 size_t numNodes = 0;
-listint_t *headPtr, *new, *add;
+long int diff;
 
-headPtr = NULL;
-while (head != NULL)
+while (head)
 {
-new = malloc(sizeof(listint_t));
-
-if (new == NULL)
-exit(98);
-
-new->p = (void *)head;
-new->next = headPtr;
-headPtr = new;
-
-add = headPtr;
-
-while (add->next != NULL)
-{
-add = add->next;
-if (head == add->p)
-{
-printf("-> [%p] %d\n", (void *)head, head->n);
-free_listint2(&headPtr);
-return (numNodes);
-}
-}
-
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
+diff = head - head->next;
 numNodes++;
+printf("[%p] %d\n", (void *)head, head->n);
+if (diff > 0)
+head = head->next;
+else
+{
+printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+break;
+}
 }
 
-free_listint2(&headPtr);
 return (numNodes);
 }
