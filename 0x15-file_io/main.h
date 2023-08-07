8ed7ce4631929aa1864e17a20570a51a2e33e0c5
Azeez Saka <saoconcept1@gmail.com>
2023-08-07 05:09:15 -0700

    add 1-create_file.c and main.h files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stddef.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -21,6 +22,7 @@ int get_endianness(void);
 
 /* Prototypes for File I/O */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
 
