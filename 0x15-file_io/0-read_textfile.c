e3265ca1ff000ba9dfad628a3c3d3e9dfe8f722e
Azeez Saka <saoconcept1@gmail.com>
2023-08-07 04:55:10 -0700

    add README.md, 0-read_textfile.c and main.h files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+* read_textfile -This function reads a text file and
+* prints it to the POSIX standard output.
+* @filename: Pointer to the name of the file to read.
+* @letters: Number of letters to read and print.
+*
+* Return: The actual number of letters read and printed. 0 on failure.
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int file_descriptor;
+ssize_t bytes_read, bytes_written;
+char *buffer;
+
+if (filename == NULL)
+return (0);
+
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
+
+file_descriptor = open(filename, O_RDONLY);
+if (file_descriptor == -1)
+{
+free(buffer);
+return (0);
+}
+
+bytes_read = read(file_descriptor, buffer, letters);
+if (bytes_read == -1)
+{
+close(file_descriptor);
+free(buffer);
+return (0);
+}
+
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+if (bytes_written == -1)
+{
+close(file_descriptor);
+free(buffer);
+return (0);
+}
+
+close(file_descriptor);
+free(buffer);
+
+return (bytes_written);
+}
+
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This is a directory for 0x15-file_io tasks on File I/O
